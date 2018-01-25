fe984e68002666c060d92789e3bc70298a2439ec
Kamalkhan Artykbayev <kamalsdu@vpn-044.umiacs.umd.edu>
2018-01-25 14:54:26 +0600

    Checking requests

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    files = {
+        'photo[0] ': (None, ' ~/kamalsdu/Desktop/img1.jpg'),
+        'photo[1] ': (None, ' ~/kamalsdu/Desktop/img2.jpg'),
+    }
+
+    response = requests.post('https://verigram.kz/api/', files=files)
+    return response.data
 
 
 if __name__ == '__main__':
